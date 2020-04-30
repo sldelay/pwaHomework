e9aa6255414efc8a4376a4581006a95516d97bac
Sam Delay <samuel.delay@gmail.com>
2020-04-30 16:57:00 -0400

    adding starter files

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const compression = require('compression')
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(compression())
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+// route for "/" to go to index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
